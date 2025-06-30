5d31c837ef2f847025d47cad5a62f01d9f9be363
Santiago Medina <ignacio.mediaz@gmail.com>
2025-06-30 01:43:33 -0400

    Update favicons and remove icon1.png reference
    Replaced dark and light mode favicon images with new versions and deleted the unused icon1.png file. Updated siteConfig to remove the explicit favicon configuration, reverting to the default favicon setup.

--- src/config.ts
+++ src/config.ts
@@ -30,11 +30,12 @@ export const siteConfig: SiteConfig = {
 		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
 	},
 	favicon: [
-		{
-		  src: '.public/favicon/icon1.png',    // Path of the favicon, relative to the /public directory
-		  theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		  sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		}
+		// Leave this array empty to use the default favicon
+		// {
+		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		// }
 	],
 };
 
