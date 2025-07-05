d6a49b5b7ac6e7a5d264d51ab9ee53f986b77554
Santiago Medina <ignacio.mediaz@gmail.com>
2025-07-05 17:17:19 -0400

    Create 1-mkdir.py

---
+++ src/content/1-mkdir.py
@@ -0,0 +1,22 @@
+import os
+import datetime
+from datetime import datetime, date
+
+month = date.today().strftime("%b")
+year = date.today().strftime("%y")
+day = datetime.now().day
+hour = datetime.now().strftime("%H")
+minute = datetime.now().strftime("%M")
+
+folder_name = f'{month}-{day}-{year}-{hour}:{minute}'
+print(folder_name)
+
+ruta = f'./src/content/posts/{folder_name}'
+
+if not os.path.exists(ruta):
+    os.makedirs(ruta)
+    print(f'Directorio {ruta} creado')
+else:
+    print(f'Directorio {ruta} ya existe')
+
+        
