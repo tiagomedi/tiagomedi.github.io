0b99ae3e8c60ed1826b7d214c34c8804d24a8427
Santiago Medina <ignacio.mediaz@gmail.com>
2025-09-10 14:40:55 -0300

    Update images and change theme color hue
    Removed dd.png, updated demo-banner.png, and added xx.png in the assets/images directory. Changed the default theme color hue from 285 to 55 in config.ts.

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "Cybersecurity writeups",
 	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
-		hue: 285, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 55, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345, 285
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
