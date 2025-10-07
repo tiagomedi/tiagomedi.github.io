ec05985e1ceca6f3ba2293e6f74dee8dfd553861
Santiago Medina <ignacio.mediaz@gmail.com>
2025-10-07 15:39:59 -0300

    Update project list and localize link labels
    Replaces the project list with new, localized entries and updates link labels in ProjectList.astro from 'Article' to 'Repositorio' and 'Demo' to 'Enlace' to better reflect the content and language preferences.

--- src/pages/projects/projects.ts
+++ src/pages/projects/projects.ts
@@ -12,54 +12,31 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    name: 'Devaradise.com',
-    description: 'A blog that sharing web development resources and tutorials',
-    demoLink: 'https://devaradise.com',
-    tags: ['Blog']
+    name: 'Full Stack Developer | Web +Arq Consultores',
+    description: 'Desarrollo e implementación del sitio web corporativo +Arq Consultores.',
+    demoLink: 'https://masarq.cl/',
+    tags: ['Landing Page', 'Corporativo']
   },
   {
-    name: 'Sellercraft App',
-    description: 'An Ecommerce omnichannel platform in Southeast Asia',
-    demoLink: 'https://sellercraft.co',
-    demoLinkRel: 'nofollow noopener noreferrer',
-    tags: ['ECommerce', 'Saas']
+    ...(await getRepositoryDetails('tiagomedi/DataScience-project')),
+    name: 'Clasificación de Notebooks por Rango de Precio.',
+    // demoLink: 'https://github.com/tiagomedi/DataScience-project',
+    postLink: 'https://github.com/tiagomedi/DataScience-project',
+    tags: ['Data Science', 'ML']
   },
   {
-    name: 'Gaji.id App',
-    description: 'Payroll and HR Management Information System',
-    demoLink: 'https://sellercraft.co',
-    demoLinkRel: 'nofollow noopener noreferrer',
-    tags: ['HRIS', 'Saas']
-  },
-  {
-    ...(await getRepositoryDetails('devaradise/paradise-ui')),
-    name: 'Paradise UI',
-    demoLink: 'https://paradise-ui.com',
-    postLink: 'https://devaradise.com/how-i-build-paradise-ui-react-component-library/',
-    tags: ['React', 'UI Library']
-  },
-  {
-    ...(await getRepositoryDetails('syakirurahman/react-lab')),
-    name: 'React Lab',
-    demoLink: 'https://devaradise.com/lab/react/',
-    tags: ['React']
-  },
-  {
-    ...(await getRepositoryDetails('syakirurahman/pokemon-catcher')),
-    name: 'Pokemon Catcher',
-    demoLink: 'https://pokemon-catcher-18636.web.app/',
-    tags: ['Hobby']
-  },
-  {
-    ...(await getRepositoryDetails('syakirurahman/movie-nominations')),
-    name: 'Movie Nominations',
-    demoLink: 'https://movie-nominations-c21c3.web.app/',
-    tags: ['Hobby']
-  },
-  {
-    ...(await getRepositoryDetails('syakirurahman/organization-tree')),
-    name: 'Organization tree',
-    demoLink: 'https://organization-tree-2a446.web.app/',
-    tags: ['Hobby']
+    ...(await getRepositoryDetails('tiagomedi/ASAI-VirtualFit')),
+    name: 'Interfaz Web de Monitoreo de Microservicios | Virtual Fit.',
+    // demoLink: 'https://github.com/tiagomedi/DataScience-project',
+    postLink: 'https://github.com/tiagomedi/ASAI-VirtualFit',
+    tags: ['SOA', 'Python', 'Docker']
   }
+  // },
+  // {
+  //   ...(await getRepositoryDetails('tiagomedi/ASAI-VirtualFit')),
+  //   name: 'Interfaz Web de Monitoreo de Microservicios | Virtual Fit.',
+  //   // demoLink: 'https://github.com/tiagomedi/DataScience-project',
+  //   postLink: 'https://github.com/tiagomedi/ASAI-VirtualFit',
+  //   tags: ['SOA', 'Python', 'Docker']
+  // }
 ]
